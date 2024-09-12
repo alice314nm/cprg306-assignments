ff7bc7f5dba6132668cc182e5d8b27af8eca2f69
Alice <alisa.ivanova@edu.sait.ca>
2024-09-12 13:02:08 -0600

    styling the header

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl pt-10 pl-10 text-rose-900">CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-2xl pt-10 pl-10 text-rose-900 font-bold">CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
         <li className="pt-5 pl-20">
           <Link href="./week-2/" className="underline text-xl text-rose-400 hover:text-rose-900">- Week-2 Assignment</Link>
