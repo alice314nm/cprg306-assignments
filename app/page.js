8cd8db1adc6713f0ec28abb01c953a3cf81af158
Alice <alisa.ivanova@edu.sait.ca>
2024-09-16 12:57:20 -0600

    week-3 preparation

--- app/page.js
+++ app/page.js
@@ -9,6 +9,9 @@ export default function Home() {
         <li className="pt-5 pl-20">
           <Link href="./week-2/" className="underline text-xl text-rose-400 hover:text-rose-900">- Week-2 Assignment</Link>
         </li>
+        <li className="pt-5 pl-20">
+          <Link href="./week-3/" className="underline text-xl text-rose-400 hover:text-rose-900">- Week-3 Assignment</Link>
+        </li>
       </ul>
     </main>
   );
