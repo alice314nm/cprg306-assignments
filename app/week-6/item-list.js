20ef8163accdba64cdfc2fc67f8500bfac83ccc0
Alice <iv.alisa21@gmail.com>
2024-10-10 15:49:31 -0600

    lab6

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -7,14 +7,30 @@ import items from './items.json';
 export default function ItemList(){
 
   const [itemList, setItemList] = useState(items);
+  const [sortBy, setSortBy] = useState("name");
   const buttonStyle="rounded bg-rose-600 p-2 hover:bg-rose-800"
+
+  function handleSort(filter) {
+    
+    let sortedItems;
+    setSortBy(filter)
+
+    if (sortBy === "name") {
+      sortedItems = items.sort((a, b) => a.name > b.name ? 1 : -1);
+    } else if (sortBy === "category") {
+      sortedItems = items.sort((a, b) => a.category > b.category ? 1 : -1);
+    }
+
+    setItemList(sortedItems);
+  }
+
+
   return (
     <div className="flex flex-col gap-5">
       <div className="ml-5 flex items-center gap-3 text-white">
         <p className="text-rose-900">Sort by:</p>
-        <button type="button" className={buttonStyle}>Name</button>
-        <button type="button" className={buttonStyle}>Category</button>
-        <button type="button" className={buttonStyle}>Grouped Category</button>
+        <button type="button" onClick={() => handleSort("name")} value="name" className={buttonStyle}>Name</button>
+        <button type="button" onClick={() => handleSort("category")} value="category" className={buttonStyle}>Category</button>
       </div>
       <div>
         <ul>
